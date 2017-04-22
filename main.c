967faef7ee23c81537f92e284e91b0d625d3ee9d
luisejrobles <luisejimenezrobles@gmail.com>
2017-04-21 18:17:45 -0700

    Comentarios corregidos

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-/*  Universidad Autonoma de Baja California
+/*  
+	Universidad Autónoma de Baja California
 	Microcontroladores y Microprocesadores
 	Jimenez Robles Luis Eduardo		01208396
-	
-	Practica 8a */
+	Práctica 8a 
+*/
 
 #include <avr/io.h>
 #define my_UBRR(bauds) 16000000/8/bauds-1
@@ -23,6 +24,9 @@ int main(void)
 	
 	UART0_getchar();
 	inMTTY();
+	
+	UART0_putchar(10);
+	UART0_putchar(13);
 	UART0_gets(cad);
 	
 	UART0_putchar(10);
@@ -35,7 +39,6 @@ int main(void)
 	UART0_putchar(13);
 	UART0_puts(cad);
 
-	
 	//==============================
 	UART0_putchar(10);
 	UART0_putchar(10);
@@ -70,7 +73,7 @@ void UART0_Init(uint16_t mode)
 char UART0_getchar(void)
 {	
 	while ( !(UCSR0A & (1<<RXC0)) );
-	
+	putchar(UDR0);
 	return UDR0;
 	
 }
@@ -89,19 +92,19 @@ void UART0_gets(char *str)
 		c = UART0_getchar();
 		if(c != 10)
 		{
-			*str = c;
-			str++;
+			*str++ = c;
+			//*str++;
 		}
 	}while(c != 10);
 	
-	*str = '\0';
+	*str++ = '\0';
 }
 
 void UART0_puts(char *str)
 {
-	while(*str++)
+	while(*str)
 	{
-		putchar(*str);
+		putchar(*str++);
 	}
 }
 
@@ -129,7 +132,33 @@ void inMTTY()
 	UART0_putchar(' ');
 	UART0_putchar('A');
 	UART0_putchar('U');
-	
+	UART0_putchar('T');
+	UART0_putchar('O');
+	UART0_putchar('N');
+	UART0_putchar('O');
+	UART0_putchar('M');
+	UART0_putchar('A');
+	UART0_putchar(' ');
+	UART0_putchar('D');
+	UART0_putchar('E');
+	UART0_putchar(' ');
+	UART0_putchar('B');
+	UART0_putchar('A');
+	UART0_putchar('J');
+	UART0_putchar('A');
+	UART0_putchar(' ');
+	UART0_putchar('C');
+	UART0_putchar('A');
+	UART0_putchar('L');
+	UART0_putchar('I');
+	UART0_putchar('F');
+	UART0_putchar('O');
+	UART0_putchar('R');
+	UART0_putchar('N');
+	UART0_putchar('I');
+	UART0_putchar('A');
+	UART0_putchar(10);
+	UART0_putchar(13);	
 	UART0_putchar('P');
 	UART0_putchar('R');
 	UART0_putchar('A');
@@ -141,7 +170,6 @@ void inMTTY()
 	UART0_putchar(' ');
 	UART0_putchar('8');
 	UART0_putchar('a');
-	
 	UART0_putchar(10);
 	UART0_putchar(13);
 }
